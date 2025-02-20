e155e2eb92d62996d9940a660721009d851b7487
Joao Palma <jdgpalma@gmail.com>
2025-02-20 15:45:16 +0000

    .

--- js/main.js
+++ js/main.js
@@ -2,6 +2,34 @@ import { isEmpty } from './utils.js';
 
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Welcome to my personal website!");
+
+    function showSection(sectionId) {
+        // Hide all sections
+        document.querySelectorAll('.content-section').forEach(section => {
+            section.classList.add('hidden');
+        });
+
+        // Show the selected section
+        document.getElementById(sectionId).classList.remove('hidden');
+    }
+
+
+    // Add event listeners to links
+    document.getElementById("edu-link").addEventListener("click", function () {
+        showSection("education");
+    });
+
+    document.getElementById("exp-link").addEventListener("click", function () {
+        showSection("experience");
+    });
+
+    document.getElementById("hob-link").addEventListener("click", function () {
+        showSection("hobbies");
+    });
+
+    document.getElementById("contact-link").addEventListener("click", function () {
+        showSection("contact");
+    });
 });
 
 
